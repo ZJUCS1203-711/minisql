0da3edf9cc5ebd422ecc69c03b5b2eac350764ac
llpngl <shanfengny@gmail.com>
2014-11-05 23:48:45 +0800

    update

--- BufferManager.h
+++ BufferManager.h
@@ -11,27 +11,9 @@
 #include "Minisql.h"
 #include <stdio.h>
 
-struct blockNode
-{
-    int offsetNum;
-    bool pin;
-    bool dirty;
-    bool reference;
-    char *address;
-    blockNode * nextBlock;
-    blockNode * preBlock;
-};
 
-struct fileNode
-{
-    char *fileName;
-    bool pin;
-   // bool is_using;
-    blockNode *blockHead;
-    fileNode * nextFile;
-    fileNode * preFile;
-};
 static int replaced_block = -1;
+
 
 class BufferManager
 {
@@ -39,21 +21,27 @@ class BufferManager
         fileNode *fileHead;
         fileNode file_pool[MAX_FILE_NUM];
         blockNode block_pool[MAX_BLOCK_NUM];
-        int total_block;
-        int total_file;
-        void init_block(blockNode &block);
-        void init_file(fileNode &file);
+        int total_block; // the number of block that have been used, which means the block is in the list.
+        int total_file; // the number of file that have been used, which means the file is in the list.
+        void init_block(blockNode & block);
+        void init_file(fileNode & file);
+        blockNode* getBlock(fileNode * file,blockNode* position,bool if_pin = false);
+        void writtenBackToDiskAll();
+        void writtenBackToDisk(const char* fileName,blockNode* block);
+        void clean_dirty(blockNode &block);
+        int getUsingSize(blockNode* block);
+    
     public:
         BufferManager();
         ~BufferManager();
         fileNode* getFile(const char* fileName,bool if_pin = false);
-        blockNode* getBlock(const char* fileName,blockNode* position,bool if_pin = false);
-        void writtenBackToDiskAll();
-        void writtenBackToDisk(const char* fileName,blockNode* block);
-        void set_dirty(blockNode &block);
-        void clean_dirty(blockNode &block);
-        void set_pin(blockNode &block,bool pin);
-        void set_pin(fileNode &file,bool pin);
+        void set_dirty(blockNode & block);
+        void set_pin(blockNode & block,bool pin);
+        void set_pin(fileNode & file,bool pin);
+    
+    blockNode* getNextBlock(fileNode * file,blockNode* block);
+    blockNode* getBlockHead(fileNode* file);
+
 };
 
 #endif /* defined(__Minisql__BufferManager__) */
