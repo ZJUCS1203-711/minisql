4410caf622ca8150c828f6e98f071c49570dc562
llpngl <shanfengny@gmail.com>
2014-11-07 22:03:20 +0800

    done api.h and api.cpp

--- API.cpp
+++ API.cpp
@@ -14,6 +14,7 @@
 
 RecordManager rm;
 CatalogManager cm;
+IndexManager im;
 
 /**
  *
@@ -22,14 +23,15 @@ CatalogManager cm;
  */
 void API::tableDrop(string tableName)
 {
-    if(cm.findFile(tableName) != TABLE_FILE)
-    {
-        cout << "There is no table " << tableName << endl;
-        return;
-    }
+    if (!tableExist(tableName)) return;
     
+    //delete a table file
     if(rm.tableDrop(tableName))
+    {
+        //delete a table information
+        cm.dropTable(tableName);
         cout << "Drop table " << tableName << " successfully" << endl;
+    }
 }
 
 /**
@@ -44,20 +46,267 @@ void API::indexDrop(string indexName)
         cout << "There is no index " << indexName << endl;
         return;
     }
+    
+    //delete a index file
     if (rm.indexDrop(indexName))
+    {
+        //delete a index information
+        cm.dropIndex(indexName);
+        
+        //delete a index tree
+        im.dropIndex(indexName);
         cout << "Drop index " << indexName << " successfully" << endl;
+    }
 }
 
-//创建一个新的index
-//输入index的名称, talbe的名称 和talbe中属性的名称
-void indexCreate(string indexName, string tableName, string attributeName)
+/**
+ *
+ * create a index
+ * @param indexName: name of index
+ * @param tableName: name of table
+ * @param attributeName: name of attribute in a table
+ */
+void API::indexCreate(string indexName, string tableName, string attributeName)
 {
-    if (cm.findFile(indexName) != INDEX_FILE)
+    if (cm.findFile(indexName) == INDEX_FILE)
     {
         cout << "There is index " << indexName << "already" << endl;
         return;
     }
     
-    cm.addIndex(indexName, tableName, attributeName);
-    cout << "Drop index " << indexName << " successfully" << endl;
+     //RecordManager to create a index file
+    if (rm.indexCreate(indexName))
+    {
+        //CatalogManager to add a index information
+        cm.addIndex(indexName, tableName, attributeName);
+        
+        //indexManager to create a index tress
+        im.indexCreate(indexName, tableName, attributeName);
+        cout << "Create index " << indexName << " successfully" << endl;
+    }
+    else
+    {
+        cout << "Create index " << indexName << " fail" << endl;
+    }
+}
+
+/**
+ *
+ * create a table
+ * @param tableName: name of table
+ * @param attributeVector: vector of attribute
+ * @param primaryKeyName: primary key of a table (default: "")
+ */
+void API::tableCreate(string tableName, vector<Attribute>* attributeVector, string primaryKeyName)
+{
+    if(cm.findFile(tableName) == TABLE_FILE)
+    {
+        cout << "There is a table " << tableName << "already" << endl;
+        return;
+    }
+    
+    //RecordManager to create a table file
+    if(rm.tableCreate(tableName))
+    {
+        //CatalogManager to create a table information
+        cm.addTable(tableName, attributeVector, primaryKeyName);
+        cout << "Create table " << tableName << " successfully" << endl;
+    }
+}
+
+/**
+ *
+ * show all record of table and the number of the record
+ * @param tableName: name of table
+ */
+void API::recordShow(string tableName)
+{
+    vector<Condition> conditionVector;
+    recordShow(tableName, &conditionVector);
+}
+
+/**
+ *
+ * show the record matching the coditions in the table and the number of the record
+ * @param tableName: name of table
+ * @param conditionVector: vector of condition
+ */
+void API::recordShow(string tableName, vector<Condition>* conditionVector)
+{
+    if (cm.findFile(tableName) == TABLE_FILE)
+    {
+        int num;
+        num = rm.recordShow(tableName, conditionVector);
+        cout << num << " records selected" << endl;
+    }
+    else
+    {
+        cout << "There is no table " << tableName << endl;
+    }
+}
+
+/**
+ *
+ * insert a record to a table
+ * @param tableName: name of table
+ * @param recordContent: Vector of these content of a record
+ */
+void API::recordInsert(string tableName, vector<string>* recordContent)
+{
+    if (!tableExist(tableName)) return;
+    
+    char recordString[2000];
+    memset(recordString, 0, 2000);
+    
+    //CatalogManager to get the record string
+    cm.recordStringGet(tableName, recordContent, recordString);
+    
+    //RecordManager to insert the record into file;
+    if(rm.recordInsert(tableName, recordString))
+    {
+         cm.insertRecord(tableName, 1);
+         cout << "insert record into table " << tableName << " successful" << endl;
+    }
+    else
+    {
+        cout << "insert record into table " << tableName << " fail" << endl;
+    }
+}
+
+/**
+ *
+ * delete all record of table
+ * @param tableName: name of table
+ */
+void API::recordDelete(string tableName)
+{
+    vector<Condition> conditionVector;
+    recordDelete(tableName, &conditionVector);
+}
+
+/**
+ *
+ * delete the record matching the coditions in the table
+ * @param tableName: name of table
+ * @param conditionVector: vector of condition
+ */
+void API::recordDelete(string tableName, vector<Condition>* conditionVector)
+{
+    if (!tableExist(tableName)) return;
+    
+    //delete record in the table file
+    int numberDelete = rm.recordDelete(tableName, conditionVector);
+    
+    //delete the number of record in in the table
+    cm.deleteValue(tableName, numberDelete);
+    cout << "delete " << numberDelete << " record in table " << tableName << endl;
+}
+
+
+/**
+ *
+ * get the number of the records in table
+ * @param tableName: name of table
+ */
+int API::recordNumGet(string tableName)
+{
+    if (!tableExist(tableName)) return 0;
+    
+    return cm.getRecordNum(tableName);
+}
+
+/**
+ *
+ * get the size of a record in table
+ * @param tableName: name of table
+ */
+int API::recordSizeGet(string tableName)
+{
+    if (!tableExist(tableName)) return 0;
+    
+    return cm.calcuteLenth(tableName);
+}
+
+/**
+ *
+ * get the size of a type
+ * @param type:  type of attribute
+ */
+int API::typeSizeGet(int type)
+{
+    return cm.calcuteLenth2(type);
+}
+
+/**
+ *
+ * get the vector of a attribute‘s name in a table
+ * @param tableName:  name of table
+ * @param attributeNameVector:  a point to vector of attributeName(which would change)
+ */
+int API::attributeNameGet(string tableName, vector<string>* attributeNameVector)
+{
+    if (tableExist(tableName)) {
+        return 0;
+    }
+    return cm.attributeNameGet(tableName, attributeNameVector);
+}
+
+
+/**
+ *
+ * get the vector of a attribute‘s type in a table
+ * @param tableName:  name of table
+ * @param attributeNameVector:  a point to vector of attributeType(which would change)
+ */
+int API::attributeTypeGet(string tableName, vector<string>* attributeTypeVector)
+{
+    if (tableExist(tableName)) {
+        return 0;
+    }
+    return cm.attributeTypeGet(tableName, attributeTypeVector);
+}
+
+
+/**
+ *
+ * get the vector of a attribute‘s type in a table
+ * @param tableName:  name of table
+ * @param attributeNameVector:  a point to vector of attributeType(which would change)
+ */
+int API::attributeGet(string tableName, vector<Attribute>* attributeVector)
+{
+    if (tableExist(tableName)) {
+        return 0;
+    }
+    return cm.attributeGet(tableName, attributeVector);
+}
+
+/**
+ *
+ * insert a value to index tree
+ * @param indexName:  name of index
+ * @param value: value that want to change
+ */
+void API::indexValueInsert(string indexName, string value)
+{
+    if(cm.findFile(indexName) != INDEX_FILE)
+    {
+         cout << "There is no index " << indexName << endl;
+        return;
+    }
+    
+    im.indexValueInsert(indexName, value);
+}
+
+int API::tableExist(string tableName)
+{
+    if (cm.findFile(tableName) != TABLE_FILE)
+    {
+        cout << "There is no table " << tableName << endl;
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
--- API.h
+++ API.h
@@ -1,14 +1,60 @@
 #ifndef API_H
 #define API_H
 #include "RecordManager.h"
-#include "IndexManager.h"
-#include "CatalogManager.h"
 
 #include "Attribute.h"
 #include "Condition.h"
 #include <string>
 #include <vector>
+#include <iostream>
 using namespace std;
+
+class IndexManager{
+public:
+    void indexCreate(string indexName, string tableName, string attributeName){return;}
+    void dropIndex(string indexName){return;}
+    void indexValueInsert(string indexName, string value){return;}
+};
+class CatalogManager
+{
+public:
+    //--------------------api need to change
+    int addIndex(string indexName,string tableName,string colName){return 1;}
+    int findFile(string fileName){return  1;}
+    int dropTable(string tableName){return 1;}
+    int dropIndex(string index){return 1;}
+    int deleteValue(string tableName){return 1;}
+    int deleteValue(string tableName, int deleteNum){return 1;}
+    int insertRecord(string tableName, int recordNum){return 1;}
+    int getRecordNum(string tableName){return 2000;}
+    
+    
+    
+    //--------------我修改比较大的
+    void addTable(string tableName, vector<Attribute>* attributeVector, string primaryKeyName = ""){}
+    
+    //使传进来的地址得到一个table的属性名称的vector
+    //输出 是否得到成功
+    //输入 table的名称，table的属性名称列表的地址
+    int attributeNameGet(string tableName, vector<string>* attributeNameVector){return 1;}
+    
+    //使传进来的地址得到一个table的属性类型的vector
+    //输出 是否得到成功
+    //输入 table的名称，table的属性类型列表的地址
+    int attributeTypeGet(string tableName, vector<string>* attributeTypeVector){return 1;}
+    
+    //使传进来的地址得到一个table的属性的vector
+    //输出 是否得到成功
+    //输入 table的名称，table的属性列表的地址
+    int attributeGet(string tableName, vector<Attribute>* attributeVector){return 1;}
+    
+    int calcuteLenth(string tableName){return 1;} //得到一个table的记录的大小
+    int calcuteLenth2(int type){return 1;}      //這理我的type改成了int类型，而不是一个String，type类型见attribute.h
+    
+    // 通过table名和recordContent得到这个table的record的字符传，写入传进的字符串
+    void recordStringGet(string tableName, vector<string>* recordContent, char* recordResult){return ;}
+};
+
 class API{
 public:
 	RecordManager rm;
@@ -16,71 +62,37 @@ public:
 	~API(){}
     
     
-	void tableDrop(string tableName);
+    void tableDrop(string tableName);
+    void tableCreate(string tableName, vector<Attribute>* attributeVector, string primaryKeyName = "no");
     
     void indexDrop(string indexName);
-    
-  
 	void indexCreate(string indexName, string tableName, string attributeName);
   
-    //创建一个新的table
-    //输入talbe的名称, 属性的一个列表 和talbe中的主键的名字
-	void tableCreate(string tableName, vector<Attribute> attributeVector, string primaryKeyName);
+    void recordShow(string tableName);
+	void recordShow(string tableName, vector<Condition>* conditionVector);
 
-    //输出 一个表的所有的记录 和 总记录数
-    //输入table的名称
-	void recordShow(string tableName);
-    
-    //输出 一个表的条件的记录 和 记录数
-    //输入table的名称, 和一个条件的列表
-	void recordShow(string tableName, vector<Condition> conditionVector);
+	void recordInsert(string tableName,vector<string>* recordContent);
 
-    //插入record
-    //输入 table的名称,和插入的这条record
-	void recordInsert(string tableName,vector<string> recordContent);
-
-    //删除 一个表的条件的所有记录
-    //输出记录数
-    //输入table的名
 	void recordDelete(string tableName);
-    
-    //删除 一个表的条件的符合条件记录
-    //输出记录数
-    //输入table的名称, 和一个条件的列表
-	void recordDelete(string tableName, vector<Condition> conditionVector);
+	void recordDelete(string tableName, vector<Condition>* conditionVector);
 
-    //输出 table的记录数
-    //输入 table的名称
 	int recordNumGet(string tableName);
-    
-    //输出 table每条记录的长度
-    //输入 table的名称
 	int recordSizeGet(string tableName);
-    
-    //输出 type每条记录的长度
-    //输入 type的名称
+
 	int typeSizeGet(int type);
     
-    //输出 table的属性名称列表
-    //输入 table的名称
-	vector<string> attributeNameGet(string tableName);
-    
-    //输出 table的属性类型列表
-    //输入 table的名称
-	vector<string> attributeTypeGet(string tableName);
-    
-    //输出 table的属性列表
-    //输入 table的名称
-    vector<Attribute> attributeGet(string tableName);
+    int attributeNameGet(string tableName, vector<string>* attributeNameVector);
+	int attributeTypeGet(string tableName, vector<string>* attributeTypeVector);
+    int attributeGet(string tableName, vector<Attribute>* attributeVector);
 
-    //插入 二叉树的一个节点（待修改）
-    //输入 索引名称,value
     void indexValueInsert(string indexName, string value);
     
     //输出 二叉树的一个节点（待修改）
     //输入 索引名称,value
     int indexBlockGet(string indexName, string value);
     
+private:
+    int tableExist(string tableName);
 };
 
 struct int_t{
