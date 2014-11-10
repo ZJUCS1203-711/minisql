d9e00ad8a2392302fbbb39aeb510236e8d681073
callMeName <rickyxuusa@gmail.com>
2014-11-10 18:55:09 +0800

    Update

--- README_testBufferManager.cpp
+++ README_testBufferManager.cpp
@@ -13,7 +13,7 @@
 #include "BufferManager.h"
 using namespace std;
 
-int main(){
+int test_bufferManager(){
     cout << "这里是BufferManager的示例程序，正式使用时请删除本程序！" << endl;
     BufferManager bm;
     const char* fileName = "Student";
@@ -28,28 +28,30 @@ int main(){
      bm.set_pin(*ftmp, true );
      */
     
-    cout << "第一个block中的using_size " << btmp -> using_size << endl;
+    cout << "第一个block中的using_size " << bm.get_usingSize(*btmp) << endl;
     
     cout << "====begin===" << endl;
-    int recordLen = 5000;
+    
+    int test = 100;
+    char* test2 = (char*)&test;
+    
+    int recordLen = sizeof(test);
     while(1)
     {
-        if(btmp->using_size <= BLOCK_SIZE - recordLen)
+        if(bm.get_usingSize(*btmp) <= bm.getBlockSize() - recordLen)
         {
             cout << "如果需要修改这个block，请使用set_dirty函数,如：bm.set_dirty(*btmp)" << endl;
             bm.set_dirty(*btmp);
             
-            cout << "修改该block前, using_size:" << btmp->using_size << endl;
-            for(int i = 0; i < recordLen; i ++)
-                strcat(btmp->address+btmp->using_size,"a");
-            
+            cout << "修改该block前, using_size:" << bm.get_usingSize(*btmp) << endl;
+            strncpy(bm.get_content(*btmp)+bm.get_usingSize(*btmp), test2, recordLen);
             cout << "修改之后，请更改该block的using_size，如：btmp->using_size += recordLen" << endl;
-            btmp->using_size += recordLen;
+            bm.set_usingSize(*btmp, bm.get_usingSize(*btmp)+recordLen);
             
             cout << "btmp -> fileName: " << btmp->fileName << endl;
             
-            cout << "修改该block后, using_size" << btmp->using_size << endl;
-            cout << "修改之后，btmp->address:" << btmp->address[1] << endl;
+            cout << "修改该block后, using_size:" << bm.get_usingSize(*btmp) << endl;
+      
             break;
         }
         else
@@ -58,9 +60,14 @@ int main(){
             btmp = bm.getNextBlock(ftmp, btmp);
         }
     }
-    
-    
     cout << "====exit==="<< endl;
-    
+   
     return 0;
 }
+
+int main()
+{
+    test_bufferManager();
+    return 0;
+}
+
