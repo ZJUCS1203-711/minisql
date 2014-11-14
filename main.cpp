7d2f05872a0bc38fb90b51802946849784348706
cristalblock <464198387@qq.com>
2014-11-14 22:34:53 +0800

    add time

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 #include "Interpreter.h"
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int main(int argc,char * argv[])
 {
+    clock_t start, finish;
+
     cout<<"*******************Welcome to use our MiniSQL**********************"<<endl;
     cout<<"******************* Author: Deng & Xu & Woo **********************"<<endl;
     int fileRead = 0;
@@ -14,6 +19,7 @@ int main(int argc,char * argv[])
     {
         if(fileRead)
         {
+
             file.open(in.fileName.c_str());
             if(!file.is_open())
             {
@@ -26,16 +32,29 @@ int main(int argc,char * argv[])
                 in.interpreter(s);
             }
             fileRead = 0;
+            finish = clock();
+            double duration = (double)(finish - start) / CLOCKS_PER_SEC;
+            duration *= 1000;
+            printf( "%2.1f seconds\n", duration );
         }
         else
         {
 
             cout<<"minisql>>";
             getline(cin,s,';');
-
+            start = clock();
+            long long haha =10000000;
+            while(haha>0)
+                haha--;
             if(in.interpreter(s)==2)
             {
                 fileRead = 1;
+            }
+            else{
+                finish = clock();
+                double duration = (double)(finish - start) / CLOCKS_PER_SEC;
+                duration *= 1000;
+                printf( "The duration is %2.1f milliseconds\n", duration );
             }
         }
 
